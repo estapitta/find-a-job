cd1882bb1bc24f87759d981ac0b9d1ac9461bd3d
estapitta <efspitta@gmail.com>
2019-01-23 08:10:00 -0600

    material ui

--- server/db/models/userJob.js
+++ server/db/models/userJob.js
@@ -1,6 +1,7 @@
 const Sequelize = require('sequelize')
 const db = require('../db')
 
+// companies the user has already seen / matched
 const UserJob = db.define('userJob', {
   matched: {
     type: Sequelize.BOOLEAN
