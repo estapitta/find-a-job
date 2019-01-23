0b7b3d3258675d4c3858f6356ab8730e951b30ce
Daniel Eimer <danieleimer1@gmail.com>
2019-01-23 14:50:08 -0600

    added single job get route

--- server/api/jobs.js
+++ server/api/jobs.js
@@ -2,6 +2,18 @@ const router = require('express').Router()
 const {Job, Company} = require('../db/models')
 module.exports = router
 
+router.get('/:jobId', async (req, res, next) => {
+  try {
+    const job = await Job.findOne({
+      where: {id: req.params.jobId},
+      include: [{model: Company}]
+    })
+    res.json(job)
+  } catch (err) {
+    next(err)
+  }
+})
+
 router.get('/', async (req, res, next) => {
   try {
     const jobs = await Job.findAll({
